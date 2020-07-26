0c5ab8b1ca220ec83a6d968d95f2628045e42800
PradeepSingh96 <pradeep.farthyal@oodlestechnologies.com>
2020-07-26 12:44:09 +0530

    commit

---
+++ evershine/users/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+# Create your models here.
+
+class Organization(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=500)
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, name, email, password=None):
+
+        if not email:
+            raise ValueError('Users Must Have an email address')
+        user = self.model(name=name, email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, name, email, password):
+
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(name, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField(max_length=254)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE,default=None, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = UserManager()
+
+
+class Otp(models.Model):
+    otp_type = models.CharField(max_length=100)
+    otp = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+
+
